0e41e43857648ca60d8462681fd340de2e0757bf
Magistry <Magistrigusti@yandex.ru>
2023-08-28 05:53:02 +0300

    0.07

--- index.js
+++ index.js
@@ -7,43 +7,40 @@ const webAppUrl = 'https://clinquant-sfogliatella-365c77.netlify.app';
 const bot = new TelegramBot(token, {polling: true});
 
 bot.on('message', async (msg) => {
-  const text = msg.text;
-  const chatId = msg.chat.id;
+    const chatId = msg.chat.id;
+    const text = msg.text;
 
-  if (text === '/start') {
-    await bot.sendMessage(chatId, 'Received your message', {
-      reply_markup: {
-        keyboard: [
-          [{text: 'fill in the form', web_app: {url: webAppUrl + '/form'}}],
-          
-        ]
-      }
-    });
+    if(text === '/start') {
+        await bot.sendMessage(chatId, 'Ниже появится кнопка, заполни форму', {
+            reply_markup: {
+                keyboard: [
+                    [{text: 'Заполнить форму', web_app: {url: webAppUrl + '/form'}}]
+                ]
+            }
+        })
 
-    await bot.sendMessage(chatId, 'button below, fill out the form', {
-      reply_markup: {
-        inline_keyboard: [
-          [{text: 'Buy TonDeV Coin', web_app: {url: webAppUrl + '/form'}}],
-        ]
-      }
-    })
-  }
-
-  if (msg?.web_app_data?.data) {
-    try {
-      const data = JSON.parse(msg?.web_app_data?.data);
-
-      await bot.sendMessage(chatId, 'thanks for the feedback');
-      await bot.sendMessage(chatId, 'your country: ' + data?.country );
-      await bot.sendMessage(chatId, 'your city: ' + data?.city);
-
-      setTimeout( async () => {
-        await bot.sendMessage(chatId, 'all information you will receive in this chat');
-      }, 3000)
-
-    } catch (e) {
-      console.log(e);
+        await bot.sendMessage(chatId, 'Заходи в наш интернет магазин по кнопке ниже', {
+            reply_markup: {
+                inline_keyboard: [
+                    [{text: 'Сделать заказ', web_app: {url: webAppUrl}}]
+                ]
+            }
+        })
     }
-  };
 
+    if(msg?.web_app_data?.data) {
+        try {
+            const data = JSON.parse(msg?.web_app_data?.data)
+            console.log(data)
+            await bot.sendMessage(chatId, 'Спасибо за обратную связь!')
+            await bot.sendMessage(chatId, 'Ваша страна: ' + data?.country);
+            await bot.sendMessage(chatId, 'Ваша улица: ' + data?.street);
+
+            setTimeout(async () => {
+                await bot.sendMessage(chatId, 'Всю информацию вы получите в этом чате');
+            }, 3000)
+        } catch (e) {
+            console.log(e);
+        }
+    }
 });
